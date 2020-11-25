7f476b629b6d2a30913039d1acefa6f474d709ba
michaelhsieh42 <michaelhsieh42@gmail.com>
2020-11-24 23:28:23 -0800

    - Fixed the data, bucket, and more.

--- sagemaker_training.r
+++ sagemaker_training.r
@@ -1,5 +1,5 @@
 # Remember to set the current working directory to where the code is
-# setwd("~/reinvent2020-aim404-productionize-r-using-amazon-sagemaker")
+setwd("~/reinvent2020-aim404-productionize-r-using-amazon-sagemaker")
 
 ###################################
 ## Getting libraries
@@ -7,6 +7,7 @@ library(reticulate)
 use_python('/usr/bin/python')
 sagemaker <- import('sagemaker')
 boto3 <- import('boto3')
+library(tsibble)
 
 # Amazon SageMaker runs your code in a container image with the dependencies.
 # We build a R container using the shell script that reads the Dockerfile
@@ -25,8 +26,11 @@ account <- boto3$client('sts')$get_caller_identity()$Account
 image <- sprintf('%s.dkr.ecr.%s.amazonaws.com/%s:%s', account, region, container, tag)
 output_path <- sprintf('s3://%s/%s', bucket, container)
 hyperparameters = list('city'='Melbourne', 'ets_trend_method' = 'A', 'ic' = 'aic')
-input_data_path <- sprintf('s3://%s/tourism_tsbl.rds', bucket)
-training_input <- sagemaker$TrainingInput(s3_data = input_data_path)
+rds_file = '/home/ubuntu/reinvent2020-aim404-productionize-r-using-amazon-sagemaker/tourism_tsbl.rds'
+saveRDS(tourism, file=rds_file)
+s3_client <- boto3$client('s3')
+s3_client$upload_file(rds_file, bucket, 'tourism_tsbl.rds')
+training_input <- sagemaker$TrainingInput(s3_data = sprintf('s3://%s/tourism_tsbl.rds', bucket))
 
 ###################################
 ## Initiate SageMaker Estimator
@@ -72,9 +76,11 @@ result=processor$run(code = 'fable_sagemaker.r',
                      wait = FALSE)
 
 ###################################
-## Execute a StepFunctions workflow by uploading a file to S3 bucket
+## Deploy AWS StepFunctions workflow
+# Please follow the "Orchestrate with AWS Step Functions" section in the README.md to 
+# deploy another CloudFormation template to setup the human review resource before proceeding
+
+###################################
+## Execute a StepFunctions workflow by uploading a file to a specific S3 bucket and prefix
 # Find the state machine at https://us-west-2.console.aws.amazon.com/states/home?region=us-west-2#/statemachines
-s3_client <- boto3$client('s3')
-s3_client$upload_file('/home/ubuntu/data-science-with-r-sagemaker.git/forecasting/data/tourism/tourism_tsbl.rds', 
-                      bucket,
-                      'data/tourism_tsbl.rds')
+s3_client$upload_file(rds_file, bucket, 'r-fable-trip-forecasting/new-data/tourism_tsbl.rds')
